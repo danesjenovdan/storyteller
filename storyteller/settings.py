1a78d2cea8ec986dbabf146c2265ba4e8db3948e
Tomaž Kunst <tomazkunst@gmail.com>
2025-11-19 13:05:35 +0100

    fixed static files

--- storyteller/settings.py
+++ storyteller/settings.py
@@ -145,7 +145,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = os.getenv("DJANGO_STATIC_URL_BASE", "static/")
+STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", os.path.join(BASE_DIR, "../static"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
